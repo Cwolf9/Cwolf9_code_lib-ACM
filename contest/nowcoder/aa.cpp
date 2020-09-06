251d7cdebdd17e10132f20ddfb66d797abeaf0eb
Cwolf9 <2717232422@qq.com>
2020-09-06 16:20:55 +0800

    try

--- contest/nowcoder/aa.cpp
+++ contest/nowcoder/aa.cpp
@@ -1,55 +1,111 @@
+/*
+**链接**
+传送门: [here]()
+**题意**
+**思路**
+**备注**
+*/
 #include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <map>
+#include <queue>
+#include <set>
+#include <deque>
+#include <list>
+#include <bitset>
+#include <complex>
+#include <cassert>
+#include <ctime>
+#define fi first
+#define se second
+#define o2(x) (x) * (x)
+#define BASE_MAX 31
+#define mk make_pair
+#define eb emplace_back
+#define SZ(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define clr(a, b) memset((a), (b), sizeof((a)))
+#define rep(i,s,t) for(register int i=s;i<t;++i)
+#define per(i,s,t) for(register int i=s;i>=t;--i)
+#define iis std::ios::sync_with_stdio(false);cin.tie(0)
+#define my_unique(x) sort(all(x)), x.erase(unique(all(x)), x.end())
 using namespace std;
-typedef long long LL;
-const int MXN = (1<<20) + 5;
-const int mod = 1000000007;
-int n, m;
-int ar[MXN], f[MXN];
-LL s[MXN], Ans[MXN], ans;
-LL ksm(LL a, LL b) {LL res = 1;for(; b; b >>= 1, a = a * a % mod) if(b & 1) res = res * a % mod;return res;}
-LL F[MXN], invF[MXN];
-void init() {
-    F[0] = invF[0] = 1;
-    for(int i = 1; i < MXN; ++i) F[i] = F[i-1] * i % mod;
-    invF[MXN - 1] = ksm(F[MXN-1], mod - 2);
-    for(int i = MXN-2; i > 0; --i) invF[i] = invF[i+1] * (i + 1) % mod;
+typedef long long int64;
+typedef unsigned long long uint64;
+typedef pair<int, int> pii;
+// mt19937 rng(time(NULL));
+// mt19937_64 rng64(chrono::steady_clock::now().time_since_epoch().count());
+// mt19937_64 generator(std::clock());
+// shuffle(arr, arr + n, generator);
+inline int64 read() {
+    int64 x = 0;int f = 0;char ch = getchar();
+    while (ch < '0' || ch > '9') f |= (ch == '-'), ch = getchar();
+    while (ch >= '0' && ch <= '9') x = (x << 3) + (x << 1) + ch - '0', ch =
+    getchar(); return x = f ? -x : x;
 }
-LL COMB(int n, int m) {
-    if(m > n) return 0;
-    if(m == n) return 1;
-    return F[n] * invF[m] % mod * invF[n - m] % mod;
+int lowbit(int x) { return x & (-x); }
+template <class T>
+T big(const T &a1, const T &a2) {return a1 > a2 ? a1 : a2;}
+template <class T>
+T sml(const T &a1, const T &a2) {return a1 < a2 ? a1 : a2;}
+template <typename T, typename... R>
+T big(const T &f, const R &... r) {return big(f, big(r...));}
+template <typename T, typename... R>
+T sml(const T &f, const R &... r) {return sml(f, sml(r...));}
+void debug_out() { cout << '\n'; }
+template <typename T, typename... R>
+void debug_out(const T &f, const R &... r) {
+    cout << f << " ";
+    debug_out(r...);
+}
+#define debug(...) cout << "[" << #__VA_ARGS__ << "]: ", debug_out(__VA_ARGS__);
+
+const int HMOD[] = {1000000009, 1004535809};
+const int64 BASE[] = {1572872831, 1971536491};
+const int64 INFLL = 0x3f3f3f3f3f3f3f3fLL;
+const int INF = 0x3f3f3f3f;
+const int mod = 998244353;// 998244353
+const int MOD = 1e9 + 7;
+const int MXN = 5e2 + 5;
+const int MXE = 2e6 + 6;
+int n, T;
+int64 dp[MXN][MXN];
+int ar[MXN];
+vector<pii> mp[MXN];
+void dfs(int u, int ba) {
+    for(int i = 0; i <= T; ++i) dp[u][i] = ar[u];
+    for(pii V: mp[u]) {
+        if(V.fi == ba) continue;
+        dfs(V.fi, u);
+        for(int i = T; i >= V.se; --i) {
+            for(int j = 0; j <= i - V.se; ++j) {
+                dp[u][i] = max(dp[u][i], dp[u][i - V.se - j] + dp[V.fi][j]);
+                // debug(u, i, j, dp[u][i])
+            }
+        }
+    }
 }
 int main() {
-    init();
-    int tim;
-    scanf("%d", &tim);
-    while(tim --) {
-        scanf("%d%d", &n, &m);
-        for(int i = 1; i <= n; ++i) {
-            scanf("%d", &ar[i]);
-            ++ f[ar[i]];
-        }
-        int sta = (1 << 20) - 1;
-        for(int i = 0; i < 20; ++i) {
-            for(int j = 0; j <= sta; ++j) {
-                if((j >> i) & 1) {
-                    f[j] += f[j - (1<<i)];
-                }
-            }
-        }
-        for(int i = 0; i <= sta; ++i) {
-            s[i] = COMB(f[i], m);
-            Ans[i] = s[i];
-        }
-        for(int i = 0; i < 20; ++i) {
-            for(int j = 0; j <= sta; ++j) {
-                if((j >> i) & 1) Ans[j] = (Ans[j] - Ans[j - (1<<i)] + mod) % mod;
-            }
-        }
-        for(int i = 0; i <= sta; ++i) ans = (ans + Ans[i] * i % mod) % mod;
-        printf("%lld\n", ans);
-        for(int i = 0; i <= sta; ++i) f[i] = Ans[i] = s[i] = 0;
-        ans = 0;
+#ifndef ONLINE_JUDGE
+    freopen("D:in.in", "r", stdin);
+    freopen("D:out.out", "w", stdout);
+#endif
+    n = read(), T = read() / 2;
+    for(int i = 1; i <= n; ++i) ar[i] = max(0LL, read());
+    for(int i = 1, a, b, c; i < n; ++i) {
+        a = read(), b = read(), c = read();
+        mp[a].eb(mk(b, c));
+        mp[b].eb(mk(a, c));
     }
+    dfs(1, 0);
+    printf("%lld\n", dp[1][T]);
+#ifndef ONLINE_JUDGE
+    // cout << "time cost:" << 1.0 * clock() / CLOCKS_PER_SEC << "ms" << endl;
+#endif
     return 0;
 }
