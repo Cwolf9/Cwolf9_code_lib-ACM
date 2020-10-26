d2fdddbd6561c98a67dc03152df50f22afad00e0
Cwolf9 <2717232422@qq.com>
2020-10-26 12:38:59 +0800

    update_zici_java

--- game/java/Main.java
+++ game/java/Main.java
@@ -1,16 +1,10 @@
 import java.util.*;
 
-public class problems {
+public class Main {
     public static void main (String[]args) {
-        Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        int []m=new int[x];
-        for(int i=0;i<x;i++) m[i]=sc.nextInt();
-        int k=0;
-        for(int i=1;i<x-1;i++) {
-            if((m[i]<m[i-1]&& m[i]<m[i+1])||(m[i]>m[i-1]&& m[i]>m[i+1]))
-                k+=1;
-        }
+        Scanner cin = new Scanner(System.in);
+        int k = cin.nextInt();
         System.out.println(k);
+        cin.close();
     }
 }
