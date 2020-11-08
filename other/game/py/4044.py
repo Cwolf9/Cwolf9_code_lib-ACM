8dff4726e65e027105b1db659db8ab22afd349d7
cwolf9 <2717232422@qq.com>
2020-11-08 17:30:20 +0800

    go

--- other/game/py/4044.py
+++ other/game/py/4044.py
@@ -6,13 +6,14 @@ def ragv():
     x = randint(0, 1048576 - 1)
     return x
 d = {}
-for i in range(220, 221): # 即在[L, R)范围内循环
+for i in range(50, 100): # 即在[L, R)范围内循环
     test_data = IO(file_prefix="", data_id=i) # 生成 heat[1|2|3].in/out 测试数据
-    tim = 20
-    test_data.input_writeln(tim)
-    n = 0
+    tim = randint(1, 100000)
+    k = randint(1, 50000)
+    test_data.input_writeln(tim, k)
     while tim > 0:
         tim = tim - 1
-        n = n + 1
-        test_data.input_writeln(n)
+        a = randint(1, 10000000)
+        test_data.input_write(a, ' ')
+    test_data.input_writeln()
     test_data.output_gen("D:\\ACM\\tmpFolder\\std.exe") # 标程编译后的可执行文件，不需要freopen等，CYaRon自动给该程序输入并获得输出作为.out
