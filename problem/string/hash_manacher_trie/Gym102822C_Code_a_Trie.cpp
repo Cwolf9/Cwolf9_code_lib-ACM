c99cd3f14f2d5e41b1cbf0a6fdc1c97ea5aa144f
cwolf9 <2717232422@qq.com>
2020-11-09 16:50:47 +0800

    AC_CCPC绵阳C题

--- problem/string/hash_manacher_trie/Gym102822C_Code_a_Trie.cpp
+++ problem/string/hash_manacher_trie/Gym102822C_Code_a_Trie.cpp
@@ -1,3 +1,55 @@
+/*
+**链接**
+传送门: [here](https://codeforces.com/gym/102822/problem/C)
+**题意**
+建议先浏览一遍题面代码。
+给你一段构建字典树的代码：每个节点随机赋予一个权值，保证每个点权值都不相同。
+再给你$n(1e5)$个字符串及其执行$query$函数后的结果，问满足这些结果的字典树最少节点数量。
+
+**思路**
+首先将字符串按照权值（即$query$后得到的答案）排序并依次插入进字典树。
+对于具有相同权值字符串，他们返回答案的节点一定在字典树$rt$到$lca$路径上的某点处。
+对于只有一个字符串的权值，其$lca$就是他自己的末尾节点。
+将所有权值对应字符串集合的$lca$标记一下，可以肯定的是每个节点只能作为一种权值的$lca$。
+
+假设有$m$个字符串权值相同，他们在$lca$后的出边是：$a,b,c,d$。
+可以肯定的是这四条出边对应节点的子树内不能有其他权值的$lca$节点。
+我们将$lca$这些出边对应的节点标记为不可能存在的节点，即$die[node]=1$。
+
+确定一下无解情况：
+- 两个字符串相同但是权值不同。
+- 多个权值对应的$lca$是同一节点。
+- $rt$到某个$lca$路径上存在$die$节点。
+
+再来确定一下$dfs$贪心求解过程，遍历到节点$u$：
+- 如果$u$的某条出边的子树内没有$lca$节点，这个子树全部扔掉。
+- 如果$u$的某条出边的子树内有多个$lca$节点，那么节点$u$必须保留。
+- 如果$u$存在若干条出边的子树内仅有$1$个$lca$节点：
+ -- 如果$u$是$lca$节点，这些子树只需要保留一个节点即可，即$u$的儿子。
+ -- 如果$u$不是$lca$节点，可以完全抛弃一个子树，剩余子树处理同上。
+
+为什么这样贪心是对的呢？一条条解答：
+- 这条出边子树内没有$lca$节点，我的$query$不会走到这里，当然可以全部抛弃掉呀。
+- 如果节点$u$不保留，代表$u$的子树都不保留，那这对应两个$lca$的$query$势必会得到同一个答案才对，互相矛盾，所以$u$必须保留。
+- 这里分了两个情况：
+-- 这些子树内只有一个$lca$节点，那么我询问的时候提前结束肯定最优，也不会影响别人，所以只需要保留$u$的儿子。
+-- 因为$u$不是$lca$，我可以把一个子树内的$lca$节点直接提到$u$来。
+
+$over!$稍微复杂点的签到题罢了。
+
+**备注**
+下标写错找了一年bug。
+```cpp
+godie(st[sid[j]], len[sid[j]]);
+---->
+godie(st[j], len[j]);
+想die哦。
+```
+
+**AC_CODE**
+思路很简单，可惜我代码写的有点冗杂。
+[here](https://github.com/Cwolf9/Cwolf9_code_lib-ACM/blob/master/problem/string/hash_manacher_trie/Gym102822C_Code_a_Trie.cpp)
+*/
 #include <bits/stdc++.h>
 #define fi first
 #define se second
@@ -62,14 +114,13 @@ const int MXE = 1e6 + 5;
 int n, m;
 char s[MXN];
 int st[MXN], len[MXN], val[MXN], sid[MXN];
-int nex[MXN][26], cnt[MXN][2], sum[MXN], islca[MXN], fk[MXN], nodeval[MXN], die[MXN];
+int nex[MXN][26], cnt[MXN][2], nodeval[MXN], sum[MXN], fk[MXN], die[MXN];
 int siz, lasnode, flag, ans;
 int new_node() {
     ++ siz;
     clr(nex[siz], -1);
-    nodeval[siz] = -1;
-    cnt[siz][0] = cnt[siz][1] = -1;
-    sum[siz] = islca[siz] = fk[siz] = die[siz] = 0;
+    nodeval[siz] = cnt[siz][0] = cnt[siz][1] = -1;
+    sum[siz] = fk[siz] = die[siz] = 0;
     return siz;
 }
 void upd_lca(int rt, int l) {
@@ -77,24 +128,22 @@ void upd_lca(int rt, int l) {
     else ++ cnt[rt][0];
     if(cnt[rt][0] >= cnt[lasnode][0]) lasnode = rt;
 }
-void add(int st, int len, int l, int val) {
+void add_str(int st, int len, int l, int val) {
     int rt = 0;
-    debug(rt)
     rep(i, 0, len) {
         upd_lca(rt, l);
         int now = s[st + i] - 'a';
         if(nex[rt][now] == -1) nex[rt][now] = new_node();
         rt = nex[rt][now];
-        debug(rt)
     }
     upd_lca(rt, l);
-    if(nodeval[rt] != -1 && nodeval[rt] != val) flag = 0;
+    if(nodeval[rt] != -1 && nodeval[rt] != val) flag = 0;//无解情况1
     nodeval[rt] = val;
 }
-void add2(int st, int len, int ip) {
+void add_lca(int st, int len) {
     if(lasnode == 0) {
-        if(islca[0] || fk[0]) flag = 0;
-        fk[0] = islca[0] = 1;
+        if(fk[0]) flag = 0;//无解情况2
+        fk[0] = 1;
         return;
     }
     int rt = 0;
@@ -103,8 +152,7 @@ void add2(int st, int len, int ip) {
         rt = nex[rt][now];
         if(rt == lasnode) break;
     }
-    if(islca[rt] || fk[rt]) flag = 0;
-    if(ip) islca[rt] = 1;
+    if(fk[rt]) flag = 0;
     fk[rt] = 1;
 }
 void godie(int st, int len) {
@@ -112,24 +160,22 @@ void godie(int st, int len) {
     rep(i, 0, len) {
         int now = s[st + i] - 'a';
         if(rt == lasnode) {
-            die[nex[rt][now]] = 1;
-            debug(st, len, rt, nex[rt][now])
+            die[nex[rt][now]] = 1;//不可能存在的节点
             break;
         }
         rt = nex[rt][now];
     }
 }
 void check(int rt) {
-    // debug(rt, sum[rt], islca[rt], fk[rt])
     sum[rt] = fk[rt];
     rep(i, 0, 26) {
         if(nex[rt][i] != -1) {
             check(nex[rt][i]);
             sum[rt] += sum[nex[rt][i]];
-            if(sum[nex[rt][i]] == 0) nex[rt][i] = -1;
+            if(sum[nex[rt][i]] == 0) nex[rt][i] = -1;//没有lca节点的子树可以直接扔掉
         }
     }
-    if(die[rt] == 1 && sum[rt]) flag = 0;
+    if(die[rt] == 1 && sum[rt]) flag = 0;//无解情况3
 }
 void dfs(int rt) {
     vector<pii> vs;
@@ -154,7 +200,7 @@ void getAns(int rt, int &ans) {
     ++ ans;
     rep(i, 0, 26) if(nex[rt][i] != -1) getAns(nex[rt][i], ans);
 }
-bool cmp(int a, int b) {
+bool cmp(const int&a, const int&b) {
     return val[a] < val[b];
 }
 int main() {
@@ -176,18 +222,18 @@ int main() {
             lenl += len[i];
             sid[i] = i;
         }
-        sort(sid, sid + n, cmp);
+        sort(sid, sid + n, cmp);//把字符串按权值排序，sid对应真实下标
         int l = 0;
         rep(ti, 0, n) {
             int i = sid[ti], ni = sid[ti + 1];
-            add(st[i], len[i], l, val[i]);
+            add_str(st[i], len[i], l, val[i]);
             // debug(i, val[i], val[ni])
             if(ti == n - 1 || val[i] != val[ni]) {
                 // debug(lasnode)
                 if(l != ti) {
                     rep(j, l, ti + 1) godie(st[sid[j]], len[sid[j]]);
                 }
-                add2(st[i], len[i], l != ti);
+                add_lca(st[i], len[i]);
                 l = ti + 1;
                 lasnode = 0;
             }
