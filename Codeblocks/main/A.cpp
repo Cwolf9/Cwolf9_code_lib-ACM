1b16742e5323f130e8b41f7d4efc00545d333d03
cwolf9 <2717232422@qq.com>
2020-11-10 16:48:34 +0800

    优化题解

--- Codeblocks/main/A.cpp
+++ Codeblocks/main/A.cpp
@@ -60,96 +60,14 @@ const int mod = 998244353;// 998244353
 const int MXN = 2e5 + 5;
 const int MXE = 1e6 + 5;
 int n, m;
-int ar[MXN], sum[MXN<<2], sumfg[MXN<<2], ans[MXN<<2], mn[MXN<<2];
-vector<int> num[MXN];
-void push_up(int rt) {
-    sum[rt] = max(sum[rt << 1], sum[rt << 1 | 1]);
-    ans[rt] = min(ans[rt << 1], ans[rt << 1 | 1]);
-}
-void build(int l, int r, int rt) {
-    sumfg[rt] = 0;
-    if(l == r) {
-        sum[rt] = ar[l];
-        ans[rt] = ar[l] - l + 1;
-        return ;
-    }
-    int mid = (l + r) >> 1;
-    build(l, mid, rt << 1), build(mid + 1, r, rt << 1 | 1);
-    push_up(rt);
-    // debug(rt, sum[rt])
-}
-void push_down(int rt, int l, int mid, int r) {
-    if(sumfg[rt] == 0) return;
-    sumfg[rt << 1] = max(sumfg[rt << 1], sumfg[rt]);
-    sumfg[rt << 1 | 1] = max(sumfg[rt << 1 | 1], sumfg[rt]);
-    if(sum[rt << 1] < sumfg[rt]) {
-        ans[rt << 1] = sumfg[rt] - mid + 1;
-        sum[rt << 1] = sumfg[rt];
-    }
-    if(sum[rt << 1 | 1] < sumfg[rt]) {
-        ans[rt << 1 | 1] = sumfg[rt] - r + 1;
-        sum[rt << 1 | 1] = sumfg[rt];
-    }
-    sumfg[rt] = 0;
-}
-void update(int L, int R, int v, int l, int r, int rt) {
-    // debug(L, R, l, r)
-    if(sum[rt] < v && L <= l && r <= R) {
-        ans[rt] = v - r + 1;
-        sum[rt] = v;
-        sumfg[rt] = max(sumfg[rt], v);
-        return;
-    }
-    if(l == r) return;
-    int mid = (l + r) >> 1;
-    push_down(rt, l, mid, r);
-    if(L > mid) {
-        update(L, R, v, mid + 1, r, rt << 1 | 1);
-    }else if(R <= mid) {
-        update(L, R, v, l, mid, rt << 1);
-    }else {
-        if(sum[rt << 1] < v) update(mid + 1, R, v, mid + 1, r, rt << 1 | 1);
-        update(L, mid, v, l, mid, rt << 1);
-    }
-    push_up(rt);
-}
+
 int main() {
 #ifndef ONLINE_JUDGE
     // freopen("/home/cwolf9/CLionProjects/mtxt/in.txt", "r", stdin);
     freopen("D:\\ACM\\mtxt\\in.txt", "r", stdin);
 #endif
     n = read(), m = read();
-    rep(i, 0, m + 1) num[i].eb(0);
-    rep(i, 1, n + 1) {
-        ar[i] = read();
-        num[ar[i]].eb(i);
-    }
-    rep(i, 1, SZ(num[1])) {
-        rep(j, num[1][i-1] + 1, num[1][i] + 1) {
-            ar[j] = num[1][i];
-            // debug(j, ar[j])
-        }
-    }
-    rep(i, num[1].back() + 1, n + 1) {
-        ar[i] = INF;
-        // debug(i, ar[i])
-    }
-    build(1, n, 1);
-    printf("%d", ans[1]);
-    rep(qi, 2, m + 1) {
-        rep(i, 1, SZ(num[qi])) {
-            int l = num[qi][i-1], r = num[qi][i];
-            update(l + 1, r, num[qi][i], 1, n, 1);
-            // debug(l + 1, r, num[qi][i])
-        }
-        if(num[qi].back() + 1 <= n) {
-            update(num[qi].back() + 1, n, INF, 1, n, 1);
-            // debug(num[qi].back() + 1, n)
-        }
-        printf(" %d", ans[1]);
-        // return 0;
-    }
-    printf("\n");
+    
 #ifndef ONLINE_JUDGE
     cout << "time cost:" << 1.0 * clock() / CLOCKS_PER_SEC << "s" << endl;
 #endif
